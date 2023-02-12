77ff703ccb67306f837f0b9964f7356bd9bc9ec8
Scott Hallock <scott.hallock@gmail.com>
2023-02-12 16:27:48 -0500

    refactor js5-2 solution

--- js5-end-to-end/challenges/js5-2-commands/app.js
+++ js5-end-to-end/challenges/js5-2-commands/app.js
@@ -4,13 +4,12 @@ const jsonParser = require('body-parser').json();
 const child_process = require('child_process');
 
 /* Serve static files (such as images) */
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/views/commands'));
 
 /* Serve the home page */
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/');
+    res.sendFile(__dirname + '/index.html');
 });
-
 
 /* Request handler for when user inputs a command */
 app.post('/', jsonParser, (req, res) => {
@@ -18,52 +17,57 @@ app.post('/', jsonParser, (req, res) => {
     const input = req.body.command.replace(/\s+/g,' ').trim().split(' ');
     const command = input[0];
     const options = input.slice(1);
+    const allowedCommands = ['cat', 'ls', 'pwd', 'git'];
 
-    const red = '#FF0000';
-    const white = '#FFFFFF';
-
-    if (command === 'git' && options[0] !== 'status') {
-        return res.json({
-            color: red,
-            message: `only 'git status' is available`
+    /* if the command is empty or not allowed */
+    if (!allowedCommands.includes(command)) {
+        return res.status(400).json({
+            color: "red",
+            message: command === '' ?
+                'please enter a command' :
+                `'${command}' is not a supported command`
         });
     }
-    if (command !== 'ls' &&
-        command !== 'cat' &&
-        command !== 'pwd' &&
-        command !== 'git') {
-        return res.json({
-            color: red,
-            message: `'${command}' is not a supported command`
+    if (command === 'cat' && options.length === 0) {
+        return res.status(400).json({
+            color: "red",
+            message: `'cat' requires 1 or more options`
+        });
+    }
+    if (command === 'git' && options[0] !== 'status') {
+        return res.status(400).json({
+            color: "red",
+            message: `only 'git status' is available`
         });
     }
     
     /* create child process */
     const child = child_process.spawn(command, options);
+    let result = '';
 
-    /* listeners for child process */
-    child.stdout.on('data', data => {
-        return res.json({
-            color: white,
-            message: data.toString()
-        });
+    /* child process listeners */
+    child.stdout.on('data', (data) => {
+        result += data.toString();
     });
-      
-    child.stderr.on('data', data => {
-        return res.json({
-            color: red,
-            message: data.toString()
-        });
+    child.stderr.on('data', (data) => {
+        result += data.toString();
     });
-
     child.on('error', error => {
-        return res.json({
-            color: red,
-            message: error.message
+        result = error.message;
+    });
+    /* wait for child process to close to send result */
+    child.on('close', code => {
+        /* code === 0 means normal exit */
+        console.log({input, code, result});
+        res.json({
+            color: code === 0 ? "white" : "red",
+            message: result
         });
     });
+
 });
 
+/* Start the server */
 app.listen(3333, () => {
-    console.log('The server has started on port 3333 ', Date.now());
+    console.log('Server is running... ', new Date());
 });
