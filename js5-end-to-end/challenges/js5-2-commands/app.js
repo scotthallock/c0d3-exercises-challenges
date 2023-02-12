3db45733211870a325f26bed5ec445ac39fcc12b
Scott Hallock <scott.hallock@gmail.com>
2023-02-11 20:14:21 -0500

    complete challenge 2

--- js5-end-to-end/challenges/js5-2-commands/app.js
+++ js5-end-to-end/challenges/js5-2-commands/app.js
@@ -1,30 +1,11 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
+const jsonParser = require('body-parser').json();
 const child_process = require('child_process');
 const { stderr } = require('process');
 
-/**
- * Create a route /commands that takes in an input box that
- * lets you execute commands directly on your server. You
- * must be able to see the response on the page.
- * 
- * Use execFile or spawn function from Node's child_process
- * library to execute the command.
- * 
- * To protect your code from malicious users, you should only
- * allow these commands: ls, cat, pwd.
- * 
- * P.S.: The child_process library also includes a exec
- * function which takes a string containing the entire
- * command to execute. This potentially may be used to
- * trigger arbitrary command execution, and so, it's a
- * unsafer alternative.
- */
-
-// const child = spawn('pwd');
-// console.log(child);
+/* Serve static files (such as images) */
+app.use(express.static(__dirname + '/'));
 
 /* Serve the home page */
 app.get('/', (req, res) => {
@@ -34,32 +15,57 @@ app.get('/', (req, res) => {
 
 /* Request handler for when user inputs a command */
 app.post('/', jsonParser, (req, res) => {
-    const input = req.body.command.trim().split(' ');
+    /* remove extra spaces from input and split */
+    const input = req.body.command.replace(/\s+/g,' ').trim().split(' ');
     const command = input[0];
     const options = input.slice(1);
 
+    const red = '#FF0000';
+    const white = '#FFFFFF';
+
+    if (command === 'git' && options[0] !== 'status') {
+        return res.json({
+            color: red,
+            message: `only 'git status' is available`
+        });
+    }
     if (command !== 'ls' &&
         command !== 'cat' &&
-        command !== 'pwd') {
-        console.log('Not a supported command.');
-        return res.send('Not a supported command.');
+        command !== 'pwd' &&
+        command !== 'git') {
+        return res.json({
+            color: red,
+            message: `'${command}' is not a supported command`
+        });
     }
     
     const child = child_process.spawn(command, options);
 
     child.stdout.on('data', data => {
-        return res.json(data.toString());
+        console.log('stdout ', data.toString());
+        return res.json({
+            color: white,
+            message: data.toString()
+        });
     });
       
     child.stderr.on('data', data => {
-        return res.json(data.toString());
+        console.log('stderr ', stderr.toString());
+        return res.json({
+            color: red,
+            message: data.toString()
+        });
     });
 
     child.on('error', error => {
-        return res.json(error.message);
+        console.log('error ', error.message);
+        return res.json({
+            color: red,
+            message: error.message
+        });
     });
 });
 
 app.listen(3333, () => {
-    console.log('The server has started on port 3333.')
+    console.log('The server has started on port 3333 ', Date.now());
 });
